9364f05eaffbcc05913e729be8906a552c4ee695
yunzivv <yunzivv@gmail.com>
2025-12-15 15:10:41 +0900

    400만 이하의 피보나치 수 중 짝수의 총합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
 
-// project Euler 1
+// project Euler 2
 public class Main {
     public static void main(String[] args) {
 
+        int before = 1;
+        int fivo = 1;
         int sum = 0;
 
-        for(int i = 1; i < 1000; i++) if(i % 3 == 0 || i % 5 == 0) sum += i;
+        while(fivo <= 4000000){
+            fivo += before;
+            before = fivo - before;
+            if(fivo % 2 == 0) sum += fivo;
+        }
 
         System.out.println(sum);
     }
