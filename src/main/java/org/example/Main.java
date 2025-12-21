de5e7d6811ebaaebc8aacd393bad645ecdc0d33a
yunzivv <yunzivv@gmail.com>
2025-12-21 15:38:33 +0900

    가장 큰 소인수 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,23 +10,13 @@ public class Main {
     public static void main(String[] args) {
 
         long num = 600851475143l;
-        long prime = 1;
+        long prime = 3;
 
-        for(long i = 2; i < num; i++) {
-            if(num % i == 0) {
-                if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0) continue;
-                if(isPrime(i))prime = i;
-            }
+        while(prime < num) {
+            if(num % prime == 0) num /= prime;
+            else prime++;
         }
 
         System.out.println("biggest prime: " + prime);
-    }
-
-    public static boolean isPrime(long n){
-        for(long i = 2; i < Math.sqrt(n); i++) {
-            if(n % i == 0) return false;
-        }
-        System.out.println("prime: " + n);
-        return true;
     }
 }
