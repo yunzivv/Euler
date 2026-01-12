b6d9b3bcdc8369b941ae9e7a377e6dc7a0c05316
yunzivv <yunzivv@gmail.com>
2026-01-12 19:08:23 +0900

    1부터 1000까지 영어로 썼을 때 사용된 글자의 개수는?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,36 @@ package org.example;
 
 import java.math.BigInteger;
 
-// project Euler 16
+// project Euler 17
 public class Main {
     public static void main(String[] args) {
 
-        BigInteger p = BigInteger.ONE;
-        int s = 0;
+        int[] one = {0,3,3,5,4,4,3,5,5,4};
+        int[] teen = {0,3,6,6,8,8,7,7,9,8,8};
+        int[] tens = {0,0,6,6,5,5,5,7,6,6};
+        int and = 3;
+        int hundred = 7;
 
-        for(int i = 1; i <= 1000; i++) {p = p.multiply(BigInteger.TWO);}
+        long sum = 0;
 
-        String str = p.toString();
-        for(char c : str.toCharArray()) {
-            s += c - '0';
+        for (int i = 1; i <= 999; i++) {
+            int n = i;
+
+            if (n >= 100) {
+                sum += one[n / 100] + hundred;
+                if (n % 100 != 0) sum += and;
+                n %= 100;
+            }
+
+            if (n >= 20) {
+                sum += tens[n / 10];
+                sum += one[n % 10];
+            } else if (n >= 10) {
+                sum += teen[n - 9];
+            } else {
+                sum += one[n];
+            }
         }
-
-        System.out.println(s);
-
+        System.out.println(sum + 11);
     }
 }
