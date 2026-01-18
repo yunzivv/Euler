24d0a7099e86cd54cc9df1521eca52686d3253d7
yunzivv <yunzivv@gmail.com>
2026-01-18 10:15:18 +0900

    20세기에서, 매월 1일이 일요일인 경우는 몇 번?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,21 @@
 package org.example;
 
-import java.math.BigInteger;
-
-// project Euler 20
+// project Euler 19
 public class Main {
     public static void main(String[] args) {
 
+        int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
         int sum = 0;
-        BigInteger h = BigInteger.ONE;
+        int sunday = 0;
+        for(int i = 1900; i < 2001; i++){
+            days[1] = ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0)) ? 29 : 28;
 
-        for(int i = 1; i <= 100; i++){
-            h = h.multiply(BigInteger.valueOf(i));
+            for(int j = 0; j < 12; j++){
+                if (i >= 1901 && sum % 7 == 6) sunday++;
+                sum = sum + days[j];
+            }
         }
 
-        for(char c : h.toString().toCharArray()){
-            sum += c - '0';
-        }
-        System.out.println(sum);
+        System.out.println(sunday);
     }
 }
-
