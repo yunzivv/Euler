cb81931ae7b112e793fd635bbafd2b2f479797a0
yunzivv <yunzivv@gmail.com>
2026-02-21 19:30:00 +0900

    1001×1001 나선모양 행렬에서 대각선 원소의 합은?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -3,21 +3,21 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-// project Euler 24
+// project Euler 28
 public class Euler {
     public static void main(String[] args) {
 
-        int[] fac = {362880, 40320, 5040, 720, 120, 24, 6, 2, 1, 1};
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        int n = 1;
+        int step = 1;
+        int cycle = 2;
 
-        int idx = 1000000 -1;
-        String s = "";
-
-        for(int i : fac){
-            s += list.remove(idx / i);
-            idx %= i;
+        for(int i = 1; i < 1001; i += 2){
+            for(int j = 0; j < 4; j++) {
+                step += cycle;
+                n += step;
+            }
+            cycle += 2;
         }
-
-        System.out.println(s);
+        System.out.println(n);
     }
 }
