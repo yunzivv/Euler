0c1f492e500bef2e3b98cead3ead33d980e3cb13
yunzivv <yunzivv@gmail.com>
2026-02-22 15:25:03 +0900

    각 자리 숫자를 5제곱해서 더했을 때 자기 자신이 되는 수들의 합은?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,20 +1,33 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
-
-// project Euler 29
+// project Euler 30
 public class Euler {
     public static void main(String[] args) {
 
-        Set<Double> set = new HashSet<>();
+        int ans = 0;
+        int max = 0;
 
-        for(int i = 2; i <= 100; i++){
-            for(int j = 2; j <= 100; j++){
-                set.add(Math.pow(i, j));
+        for (int n = 1; n < 20; n++) {
+
+            double left = Math.pow(10, n - 1);
+            double right = n * Math.pow(9, 5);
+
+            if (left <= right) {
+                max = n;
             }
         }
 
-        System.out.println(set.size());
+        max *= (int)Math.pow(9, 5);
+
+        for(int i = 10; i < max; i++) {
+            String s = String.valueOf(i);
+            int sum = 0;
+            for(char c : s.toCharArray()) {
+                sum += (int) Math.pow(c - '0', 5);
+            }
+            if(sum == i) ans += i;
+        }
+
+        System.out.println(ans);
     }
 }
