39ed0dd578710b378321cf3d83a65eafe661f4cc
yunzivv <yunzivv@gmail.com>
2026-02-28 19:34:12 +0900

    각 자릿수의 팩토리얼을 더했을 때 자기 자신이 되는 수들의 합은?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,39 +1,23 @@
 package org.example;
 
-// project Euler 33
+// project Euler 34
 public class Euler {
     public static void main(String[] args) {
 
-        long m = 1;
-        long s = 1;
+        int ans = 0;
+        int[] facts = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 
-        for (int i = 20; i <= 99; i++) {
-            for (int j = 10; j < i; j++) {
-
-                int it = i / 10, io = i % 10;
-                int jt = j / 10, jo = j % 10;
-
-                boolean ok = false;
-
-                if (io == jt && io != 0) ok |= (long)j * it == (long)i * jo;
-                if (it == jo && it != 0) ok |= (long)j * io == (long)i * jt;
-                if (io == jo && io != 0) ok |= (long)j * it == (long)i * jt;
-                if (it == jt && it != 0) ok |= (long)j * io == (long)i * jo;
-
-                if (ok) {
-                    m *= i;
-                    s *= j;
-                }
+        for(int i = 3; i < 10000000; i++){
+            int sum = 0;
+            int t = i;
+            while(t > 0){
+                sum += facts[t % 10];
+                t /= 10;
             }
+
+            if(i == sum) ans += i;
         }
 
-        for(int i = 2; i <= m; i++){
-            while(m % i == 0 && s % i == 0){
-                m /= i;
-                s /= i;
-            }
-        }
-
-        System.out.println(m);
+        System.out.println(ans);
     }
 }
