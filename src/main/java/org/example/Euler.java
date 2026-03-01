885b533333a44b25f04beaf52d293aaa41ae6b4b
yunzivv <yunzivv@gmail.com>
2026-03-01 19:34:12 +0900

    백만 미만인 원형 소수 개수 구하기

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -4,20 +4,34 @@ package org.example;
 public class Euler {
     public static void main(String[] args) {
 
-        int ans = 0;
-        int[] facts = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+        int ans = 13;
+        for(int i = 100; i < 1_000_000; i++){
+            if(isPrime(i)){
+                String s = Integer.toString(i);
+                boolean cyclePrime = true;
+                for (int j = 0; j < s.length(); j++) {
+                    s = s.substring(1) + s.charAt(0);
+                    if(!isPrime(Integer.parseInt(s))) {
+                        cyclePrime = false;
+                        break;
+                    }
+                }
 
-        for(int i = 3; i < 10000000; i++){
-            int sum = 0;
-            int t = i;
-            while(t > 0){
-                sum += facts[t % 10];
-                t /= 10;
+                if(cyclePrime) ans++;
             }
-
-            if(i == sum) ans += i;
         }
 
         System.out.println(ans);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 }
