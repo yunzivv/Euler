4bb8ad9919b04df327f767ca2d8c2f5fa41be0f0
yunzivv <yunzivv@gmail.com>
2026-03-14 21:32:12 +0900

    1~9 팬디지털 곱셈식을 만들 수 있는 모든 수의 합

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,28 +1,66 @@
 package org.example;
 
-// project Euler 39
+import java.util.HashSet;
+import java.util.Set;
+
+// project Euler 32
 public class Euler {
     public static void main(String[] args) {
 
         int answer = 0;
-        int maxCnt = 0;
+        Set<Integer> pandigital = new HashSet<>();
 
-        for (int l = 12; l <= 1000; l++) {
-            int cnt = 0;
+        for(int a = 1; a <= 9; a++){
 
-            for(int a = 1; a <= l/3; a++){
-                for(int b = 1; b <= l/2; b++){
-                    int c = l - a - b;
-                    if(Math.pow(c, 2) == a * a + b * b) cnt++;
-                }
-            }
+            for(int b = 1023; b <= 9876; b++){
 
-            if(maxCnt < cnt){
-                answer = l;
-                maxCnt = cnt;
+                int c = a * b;
+
+                int mask = 1 << a;
+                mask = pandigital(mask, b);
+                if(mask == -1) continue;
+                mask = pandigital(mask, c);
+                if(mask == -1) continue;
+
+                if(mask == 0b1111111110) pandigital.add(c);
             }
         }
 
+        for(int a = 12; a <= 99; a++){
+
+            for(int b = 102; b <= 987; b++){
+                int c = a * b;
+
+                int mask = pandigital(0, a);
+                if(mask == -1) continue;
+
+                mask = pandigital(mask, b);
+                if(mask == -1) continue;
+
+                mask = pandigital(mask, c);
+                if(mask == -1) continue;
+
+                if(mask == 0b1111111110) pandigital.add(c);
+            }
+        }
+
+        for(int a : pandigital){
+            answer += a;
+        }
+
         System.out.println(answer);
+    }
+
+    static int pandigital(int mask, int n){
+        while(n > 0){
+            int d = n % 10;
+
+            if(d == 0) return -1;
+            if((mask & (1 << d)) != 0) return -1;
+
+            mask |= 1 << d;
+            n /= 10;
+        }
+        return mask;
     }
 }
