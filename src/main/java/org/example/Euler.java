822bd2d376003d8f816d52cbf20763d90c3438eb
yunzivv <yunzivv@gmail.com>
2026-03-15 11:12:12 +0900

    어떤 수에 (1, 2, ... )를 곱해서 이어붙여 얻을 수 있는 가장 큰 1 ~ 9 팬디지털 수

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -3,64 +3,35 @@ package org.example;
 import java.util.HashSet;
 import java.util.Set;
 
-// project Euler 32
+// project Euler 38
 public class Euler {
     public static void main(String[] args) {
 
         int answer = 0;
-        Set<Integer> pandigital = new HashSet<>();
 
-        for(int a = 1; a <= 9; a++){
+        for(int i = 9; i < 9999; i++){
 
-            for(int b = 1023; b <= 9876; b++){
-
-                int c = a * b;
-
-                int mask = 1 << a;
-                mask = pandigital(mask, b);
-                if(mask == -1) continue;
-                mask = pandigital(mask, c);
-                if(mask == -1) continue;
-
-                if(mask == 0b1111111110) pandigital.add(c);
+            String s = "";
+            int[] alpha = new int[10];
+            for(int j = 1; j <= 9; j++){
+                s += i * j;
+                if(s.length() >= 9) break;
             }
-        }
 
-        for(int a = 12; a <= 99; a++){
+            if(s.length() == 9){
+                boolean ok = true;
+                for(char c : s.toCharArray()){
+                    if(alpha[c - '0'] != 0 || c == '0') {
+                        ok = false;
+                        break;
+                    }
+                    alpha[c - '0']++;
+                }
 
-            for(int b = 102; b <= 987; b++){
-                int c = a * b;
-
-                int mask = pandigital(0, a);
-                if(mask == -1) continue;
-
-                mask = pandigital(mask, b);
-                if(mask == -1) continue;
-
-                mask = pandigital(mask, c);
-                if(mask == -1) continue;
-
-                if(mask == 0b1111111110) pandigital.add(c);
+                if(ok) answer = Math.max(Integer.parseInt(s), answer);
             }
-        }
-
-        for(int a : pandigital){
-            answer += a;
         }
 
         System.out.println(answer);
-    }
-
-    static int pandigital(int mask, int n){
-        while(n > 0){
-            int d = n % 10;
-
-            if(d == 0) return -1;
-            if((mask & (1 << d)) != 0) return -1;
-
-            mask |= 1 << d;
-            n /= 10;
-        }
-        return mask;
     }
 }
