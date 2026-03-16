e8b3ee2140b31a6979628d4a8477519da708cbea
yunzivv <yunzivv@gmail.com>
2026-03-16 11:12:12 +0900

    왼쪽이나 오른쪽에서 한자리씩 없애가도 여전히 소수인 수의 합은?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -8,30 +8,52 @@ public class Euler {
     public static void main(String[] args) {
 
         int answer = 0;
+        int cnt = 0;
+        int num = 35;
 
-        for(int i = 9; i < 9999; i++){
+        while(cnt < 11){
+            num += 2;
 
-            String s = "";
-            int[] alpha = new int[10];
-            for(int j = 1; j <= 9; j++){
-                s += i * j;
-                if(s.length() >= 9) break;
+            int n = num;
+            boolean ok = isPrime(n);
+
+            if(ok) {
+                while (n >= 10 && ok) {
+                    n /= 10;
+                    if(!isPrime(n)) ok = false;
+                }
             }
 
-            if(s.length() == 9){
-                boolean ok = true;
-                for(char c : s.toCharArray()){
-                    if(alpha[c - '0'] != 0 || c == '0') {
-                        ok = false;
-                        break;
-                    }
-                    alpha[c - '0']++;
-                }
+            if(ok){
+                n = num;
+                int pow = 1;
+                while(pow <= n) pow *= 10;
+                pow /= 10;
 
-                if(ok) answer = Math.max(Integer.parseInt(s), answer);
+                while (n >= 10 && ok) {
+                    n %= pow;
+                    if(!isPrime(n)) ok = false;
+                    pow /= 10;
+                }
+            }
+
+            if(ok) {
+                cnt++;
+                answer += num;
             }
         }
 
         System.out.println(answer);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 }
