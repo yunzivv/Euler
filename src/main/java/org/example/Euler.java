f86489c001c8372919899301d37d85cf7cbf518c
yunzivv <yunzivv@gmail.com>
2026-03-17 14:19:04 +0900

    10진법과 2진법으로 모두 대칭수인 1,000,000 미만 수의 합

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,59 +1,30 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
-
-// project Euler 38
+// project Euler 36
 public class Euler {
     public static void main(String[] args) {
 
         int answer = 0;
-        int cnt = 0;
-        int num = 35;
 
-        while(cnt < 11){
-            num += 2;
-
-            int n = num;
-            boolean ok = isPrime(n);
-
-            if(ok) {
-                while (n >= 10 && ok) {
-                    n /= 10;
-                    if(!isPrime(n)) ok = false;
-                }
-            }
-
-            if(ok){
-                n = num;
-                int pow = 1;
-                while(pow <= n) pow *= 10;
-                pow /= 10;
-
-                while (n >= 10 && ok) {
-                    n %= pow;
-                    if(!isPrime(n)) ok = false;
-                    pow /= 10;
-                }
-            }
-
-            if(ok) {
-                cnt++;
-                answer += num;
-            }
+        for(int i = 1; i < 1000000; i++){
+            if(isPalindrome(i)) answer += i;
         }
 
         System.out.println(answer);
     }
 
-    public static boolean isPrime(int n) {
-        if (n < 2) return false;
-        if (n <= 3) return true;
-        if (n % 2 == 0) return false;
+    static boolean isPalindrome(int n){
 
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
+        String ten = Integer.toString(n);
+        for(int i = 0; i < ten.length() / 2; i++){
+            if(ten.charAt(i) != ten.charAt(ten.length() - 1 - i)) return false;
         }
+        String two = Integer.toBinaryString(n);
+        for(int i = 0; i < two.length() / 2; i++){
+            if(two.charAt(i) != two.charAt(two.length() - 1 - i)) return false;
+        }
+
         return true;
     }
+
 }
