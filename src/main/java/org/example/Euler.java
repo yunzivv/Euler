d44ff2e4a832e27618e482fc839e81fd53032bac
yunzivv <yunzivv@gmail.com>
2026-03-19 16:55:54 +0900

    (소수 + 2×제곱수)로 나타내지 못하는 가장 작은 홀수인 합성수는?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,24 +1,43 @@
 package org.example;
 
-import java.io.IOException;
 
-// project Euler 44
+// project Euler 46
 public class Euler {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        long n = 40756;
+        int answer = 0;
+        int n = 0;
+
+        int[] primes = new int[1000];
+
+        for(int i = 2; i < 10000; i++){
+            if(n == 1000) break;
+            if(Util.isPrime(i)) primes[n++] = i;
+        }
+
+        n = 35;
 
         while(true){
-            if(Util.getTriangularIndex(n) != -1
-            && Util.getPentagonalIndex(n) != -1
-            && Util.getHexagonalIndex(n) != -1){
+            n += 2;
+            boolean ok = true;
+            if(Util.isPrime(n)) continue;
+
+            for(int i = 0; i < 1000; i++){
+                if(n <= primes[i]) break;
+
+                double m = Math.sqrt((n - primes[i]) / 2.0);
+                if(m % 1 == 0) {
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) {
                 System.out.println(n);
                 break;
             }
-
-            n++;
         }
 
-    }
+//        System.out.println(answer);
 
+    }
 }
