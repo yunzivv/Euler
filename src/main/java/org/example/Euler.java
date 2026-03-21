2ff49d85a32c8437678860ff5d9d884a2065f1b8
yunzivv <yunzivv@gmail.com>
2026-03-21 11:12:12 +0900

    1백만 이하의 소수 중 가장 길게 연속되는 소수의 합으로 표현되는 수는?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,56 +1,45 @@
 package org.example;
 
-
 import java.util.*;
 
-// project Euler 48
 public class Euler {
     public static void main(String[] args) {
 
-        List<Integer> primes = new ArrayList<>();
-        for (int i = 1235; i < 10000; i += 2) {
+        List<Integer> list = new ArrayList<>();
+
+        // 소수 리스트
+        list.add(2);
+        for (int i = 3; i < 1000000; i += 2) {
             if (Util.isPrime(i)) {
-                primes.add(i);
+                list.add(i);
             }
         }
 
-        // 순열 기준으로 그룹 묶기
-        Map<String, List<Integer>> map = new HashMap<>();
+        int answer = 0;
+        int len = 0;
 
-        for (int num : primes) {
-            char[] arr = String.valueOf(num).toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
+        for (int start = 0; start < list.size(); start++) {
 
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(num);
-        }
+            int sum = 0;
 
-        // 각 그룹에서 등차수열 찾기
-        for (List<Integer> group : map.values()) {
+            // end 늘려가면서 합 구하기
+            for (int end = start; end < list.size(); end++) {
 
-            if (group.size() < 3) continue;
+                sum += list.get(end);
+                if (sum >= 1000000) break;
 
-            Collections.sort(group);
+                // 소수 + 길이 체크
+                if (Util.isPrime(sum)) {
+                    int curLen = end - start + 1;
 
-            int size = group.size();
-
-            for (int i = 0; i < size; i++) {
-                for (int j = i + 1; j < size; j++) {
-
-                    int a = group.get(i);
-                    int b = group.get(j);
-                    int c = 2 * b - a;
-
-                    if (group.contains(c)) {
-                        if (a == 1487 && b == 4817) continue;
-
-                        System.out.println("" + a + b + c);
+                    if (curLen > len) {
+                        len = curLen;
+                        answer = sum;
                     }
                 }
             }
         }
 
+        System.out.println(answer);
     }
-
 }
