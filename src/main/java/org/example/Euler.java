dda2230dafecd260dad31e8c3839865b919da756
yunzivv <yunzivv@gmail.com>
2026-03-23 23:28:42 +0900

    1 ≤ n ≤ 100 일때 nCr의 값이 1백만을 넘는 경우는 모두 몇 번?

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,35 +1,23 @@
 package org.example;
 
-import java.util.*;
+// Euler 53
 
 public class Euler {
     public static void main(String[] args) {
 
-        int answer = 123;
+        int answer = 0;
 
-        while(true){
-            answer++;
+        for (int n = 23; n <= 100; n++) {
+            long comb = 1;
 
-            // 1-6배의 각 자리 수를 담을 배열
-            char[][] nums = new char[6][String.valueOf(answer).length()];
+            for (int r = 1; r <= n / 2; r++) {
+                comb = comb * (n - r + 1) / r;
 
-            for(int i = 1; i <= 6; i++){
-                nums[i - 1] = String.valueOf(answer * i).toCharArray();
-                // 정렬
-                Arrays.sort(nums[i - 1]);
-            }
-
-            // 정렬 후 같은 숫자로 이루어지는 지 확인
-            boolean ok = false;
-            for(int i = 0; i < String.valueOf(answer).length(); i++){
-                if(nums[0][i] != nums[1][i] || nums[2][i] != nums[3][i] || nums[4][i] != nums[5][i]
-                || nums[0][i] != nums[2][i] || nums[0][i] != nums[4][i]){
-                    ok = true;
+                if (comb > 1_000_000) {
+                    answer += (n - 2 * r + 1);
                     break;
                 }
             }
-
-            if(!ok) break;
         }
 
         System.out.println(answer);
