d950cf0d01ccc8c67c3b81bcb0a3c975ed11815e
yunzivv <yunzivv@gmail.com>
2026-03-28 19:42:12 +0900

    XOR 방식으로 암호화된 메시지 깨기

--- src/main/java/org/example/Euler.java
+++ src/main/java/org/example/Euler.java
@@ -1,31 +1,49 @@
 package org.example;
 
-// Euler 58
+// Euler 59
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Euler {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int n = 1;
-        int step = 2;
+        int answer = 0;
 
-        int primeCnt = 0;
-        int totalCnt = 1;
+        String password = Files.readString(Path.of("text.txt"));
+        String[] split = password.split(",");
+        int[] letter = new int[split.length];
 
-        while(true){
-            for(int i = 0; i < 4; i++){
-                n += step;
-                if(Util.isPrime(n)) primeCnt++;
-            }
-
-            totalCnt += 4;
-
-            double ratio = (double) primeCnt / totalCnt;
-
-            if(ratio < 0.1) break;
-
-            step += 2;
+        for (int i = 0; i < split.length; i++) {
+            letter[i] = Integer.parseInt(split[i].trim());
         }
 
-        System.out.println(step + 1);
+        for (char a = 'a'; a <= 'z'; a++) {
+            for (char b = 'a'; b <= 'z'; b++) {
+                for (char c = 'a'; c <= 'z'; c++) {
+
+                    char[] key = {a, b, c};
+                    StringBuilder decoded = new StringBuilder();
+
+                    for (int i = 0; i < letter.length; i++) {
+                        char ch = (char)(letter[i] ^ key[i % 3]);
+                        decoded.append(ch);
+                    }
+
+                    String res = decoded.toString();
+
+                    if (res.contains(" the ")) {
+                        System.out.println("" + a + b + c);
+                        for(char r : res.toCharArray()){
+                            answer += r;
+                        }
+                        System.out.println(answer);
+                        return;
+                    }
+                }
+            }
+        }
+
     }
 }
